0f0782831895ce5eae3e9b7d573ce3e279cd23e7
Rui Ramos <rui@ometria.com>
2014-07-20 02:02:14 +0100

    relative paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
         },
         'gh-pages': {
             options: {
-                clone: 'bower_components/my-repo'
+                clone: 'bower_components/x-mixtaper'
             },
             src: [
                 'bower_components/**/*',
